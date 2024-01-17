78ab5b5f004b537eba7d73057585f778fe8b5f0c
Jack Radford <jack.radford@live.co.uk>
2024-01-17 17:07:51 +0000

    Refactored the PersistenceController.

--- Focus/Models/Persistence.swift
+++ Focus/Models/Persistence.swift
@@ -9,33 +9,9 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        let calendar = Calendar.current
-        for index in 0..<500 {
-            let end = calendar.date(byAdding: .hour, value: -4 * index, to: .now) ?? Date()
-            let newSession = Session(context: viewContext)
-            newSession.id = UUID()
-            newSession.startDate = calendar.date(byAdding: .minute, value: -Int.random(in: 5..<121), to: end)
-            newSession.endDate = end
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
     
-    static let previewMoc = preview.container.viewContext
-
     let container: NSPersistentCloudKitContainer
-
+    
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "Focus")
         if inMemory {
@@ -45,7 +21,7 @@ struct PersistenceController {
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -58,5 +34,40 @@ struct PersistenceController {
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
+
+// MARK: - Preview
+extension PersistenceController {
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        generatePreviewDate(viewContext)
+        return result
+    }()
+    
+    static let previewMoc = preview.container.viewContext
+    
+    /// Generates some Sessions and saves them to the given viewContext.
+    ///
+    /// - Parameter viewContext: The NSManagedObjectContext to use.
+    static private func generatePreviewDate(_ viewContext: NSManagedObjectContext) {
+        let calendar = Calendar.current
+        for index in 0..<500 {
+            let end = calendar.date(byAdding: .hour, value: -4 * index, to: .now) ?? Date()
+            let newSession = Session(context: viewContext)
+            newSession.id = UUID()
+            newSession.startDate = calendar.date(byAdding: .minute, value: -Int.random(in: 5..<121), to: end)
+            newSession.endDate = end
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
     }
 }
