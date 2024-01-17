3ee69173a9ad440843d821c98212206edd68e2fc
Jack Radford <jack.radford@live.co.uk>
2024-01-17 16:55:25 +0000

    Improved timeStringFrom documentation.

--- Focus/Utils/Formatters.swift
+++ Focus/Utils/Formatters.swift
@@ -7,15 +7,19 @@
 
 import Foundation
 
-/// Get formatted time string from a TimeInterval (a.k.a. Double) `diff`.
-/// Format depends on `allowedUnits`.
-/// Units are shown if `showUnits` is true
+/// Get formatted time string from a TimeInterval
+///
+/// - Parameters:
+///   - diff: Time Interval.
+///   - showUnits: Bool dictating if units should be shown or not.
+///   - allowedUnits: For the DateComponentsFormatter
+/// - Returns: A formatted time String.
 func timeStringFrom(diff: TimeInterval, showUnits: Bool, allowedUnits: NSCalendar.Unit) -> String {
     let formatter = DateComponentsFormatter()
     formatter.allowedUnits = allowedUnits
     if showUnits {
         formatter.unitsStyle = .abbreviated
     }
-
+    
     return formatter.string(from: diff) ?? ""
 }
